83706a8de87c1ba34dd7193347ecd6ddc42cacc2
newming <a977527479@sina.com>
2017-02-11 10:15:24 +0800

    fix bugs

--- example/client/src/App.js
+++ example/client/src/App.js
@@ -3,16 +3,14 @@ import Header from './Header';
 import Footer from './Footer';
 import Table from './Table';
 
-class App extends React.Component {
-  render () {
-    return(
-      <div>
-        <Header />
-        <Table />
-        <Footer />
-      </div>
-    )
-  }
+function App() {
+  return(
+    <div>
+      <Header />
+      <Table />
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
--- example/client/src/Form.js
+++ example/client/src/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import {api} from './config';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Form extends React.Component{
   constructor(){
@@ -23,27 +24,55 @@ class Form extends React.Component{
     let age = this.refs.age.value;
     let male = this.state.maleValue;
     let email = this.refs.email.value;
-    let postData = {name,age,male,email};
-    this.props.action(postData,this.props.type);
+    if (this.checkName(name)&&this.checkAge(age)&&this.checkEmail(email)) {
+      let postData = {name,age,male,email};
+      this.props.action(postData,this.props.type);
+    }else {
+      alert('请核对表单内容')
+    }
   }
   handleShow(){
     this.setState({
-      show: !this.state.show
+      show: !this.state.show,
+      name: null,
+      age: null,
+      email: null
     })
   }
   handleBlur(e){
     let _id = e.target.getAttribute('id');
     let _target = document.getElementById(_id);
     let _value = _target.value.trim();
-    if (_id === 'name') {
-      if (_value.length===0) {this.setState({name: '姓名不能为空'})}else { this.setState({name: null}) }
+    if (_id === 'name') { this.checkName(_value) }
+    if (_id === 'age') { this.checkAge(_value) }
+    if(_id === 'email') { this.checkEmail(_value) }
+  }
+  checkName(_value) {
+    if (_value.length===0) {
+      this.setState({name: '姓名不能为空'});
+      return false;
+    }else {
+      this.setState({name: null});
+      return true;
     }
-    if (_id === 'age') {
-      if (Math.floor(_value) == _value && _value > 0) {this.setState({age:null})}else{ this.setState({age: '请输入一个大于0的整数'}) }
+  }
+  checkAge(_value) {
+    if (Math.floor(_value) == _value && _value > 0) {
+      this.setState({age:null});
+      return true;
+    }else{
+      this.setState({age: '请输入一个大于0的整数'})
+      return false;
     }
-    if(_id === 'email') {
-      let re= /\w@\w*\.\w/;
-      if (re.test(_value)) { this.setState({email: null}) }else { this.setState({email: '请输入正确的邮箱格式'}) }
+  }
+  checkEmail(_value) {
+    let re= /\w@\w*\.\w/;
+    if (re.test(_value)) {
+      this.setState({email: null})
+      return true
+    }else {
+      this.setState({email: '请输入正确的邮箱格式'})
+      return false
     }
   }
   render(){
@@ -98,7 +127,12 @@ class Form extends React.Component{
       : null
     return(
       <div>
-        {content}
+        <ReactCSSTransitionGroup
+          transitionName="formani"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+          {content}
+        </ReactCSSTransitionGroup>
       </div>
     )
   }
